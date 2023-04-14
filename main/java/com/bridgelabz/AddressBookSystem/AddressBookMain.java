774fdb02e13064d9c7a7408e1857a1a4125f664b
sanjana8100 <sanjanams812@gmail.com>
2023-04-14 12:26:40 +0530

    added: UC15

--- main/java/com/bridgelabz/AddressBookSystem/AddressBookMain.java
+++ main/java/com/bridgelabz/AddressBookSystem/AddressBookMain.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.AddressBookSystem;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvException;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -204,7 +211,7 @@ public class AddressBookMain {
         }
     }
 
-    public static void writeToCSVFile() {
+    private static void writeToCSVFile() {
         FileWriter fileWriter = null;
         String csvPath = "C:\\Users\\INS 5570\\Desktop\\AddressBooks.csv";
 
@@ -230,7 +237,7 @@ public class AddressBookMain {
         }
     }
 
-    public static void readFromCSVFile(){
+    private static void readFromCSVFile(){
         String csvPath = "C:\\Users\\INS 5570\\Desktop\\AddressBooks.csv";
         FileReader fileReader = null;
         try {
@@ -251,7 +258,7 @@ public class AddressBookMain {
             e.printStackTrace();
         }
 
-        System.out.println("\nReading data from csv file:");
+        System.out.println("\nReading from : " + csvPath + "\n");
         linesOfData.stream().forEach(lines -> {
             for (String value : lines)
                 System.out.print(value + "\t");
@@ -262,6 +269,63 @@ public class AddressBookMain {
             reader.close();
         } catch (IOException e) {
 
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeToJSONFile(){
+        String jsonPath = "C:\\Users\\INS 5570\\IdeaProjects\\UpdatedAddressBookUsingCSVandJSON\\src\\main\\java\\com\\bridgelabz\\AddressBookSystem\\AddressBooks.json";
+
+        try {
+            FileWriter fileWriter = new FileWriter(jsonPath);
+            Gson gson = new GsonBuilder().registerTypeAdapter(Contact.class,
+                    new TypeAdapter<String>() {
+                        @Override
+                        public void write(JsonWriter jsonWriter, String contact) throws IOException{
+                            jsonWriter.value(contact.toString());
+                        }
+                        @Override
+                        public String read(JsonReader jsonReader) throws IOException{
+                            return jsonReader.nextString();
+                        }
+                    }).create();
+            addressBookMap.values().stream().forEach(contact -> {
+                String contactDataString = contact.toString().concat("\n");
+                gson.toJson(contactDataString,fileWriter);
+            });
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readFromJSONFile(){
+        String jsonPath = "C:\\Users\\INS 5570\\IdeaProjects\\UpdatedAddressBookUsingCSVandJSON\\src\\main\\java\\com\\bridgelabz\\AddressBookSystem\\AddressBooks.json";
+
+        try {
+            FileReader fileReader = new FileReader(jsonPath);
+            Type type = new TypeToken<HashMap<String,AddressBook>>(){}.getType();
+            Gson gson = new GsonBuilder().registerTypeAdapter(Contact.class,
+                    new TypeAdapter<String>() {
+                        @Override
+                        public void write(JsonWriter jsonWriter, String contact) throws IOException{
+                             jsonWriter.value(contact.toString());
+                        }
+                        @Override
+                        public String read(JsonReader jsonReader) throws IOException{
+                            return jsonReader.nextString();
+                        }
+                    }).create();
+            System.out.println("Reading from : " + jsonPath + "\n");
+            List<String> theList = gson.fromJson(fileReader,type);
+            fileReader.close();
+            for (String contact : theList){
+                System.out.println(contact.toString());
+            }
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
@@ -283,7 +347,8 @@ public class AddressBookMain {
             System.out.println("=> To Display Contacts in an Address Book in Sorted Order based on a specific detail: PRESS 8");
             System.out.println("=> To Read and Display all the Contacts from the Address Book File: PRESS 9");
             System.out.println("=> To Read and Display all the Contacts from the Address Book CSV File: PRESS 10");
-            System.out.println("=> To EXIT: PRESS 11");
+            System.out.println("=> To Read and Display all the Contacts from the Address Book JSON File: PRESS 11");
+            System.out.println("=> To EXIT: PRESS 12");
             int choice = in.nextInt();
 
             switch (choice) {
@@ -291,12 +356,14 @@ public class AddressBookMain {
                     addAddressBook();
                     writeToFile();
                     writeToCSVFile();
+                    writeToJSONFile();
                     System.out.println();
                     break;
                 case 2:
                     addContacts();
                     writeToFile();
                     writeToCSVFile();
+                    writeToJSONFile();
                     break;
                 case 3:
                     editContact();
@@ -321,6 +388,9 @@ public class AddressBookMain {
                     break;
                 case 10:
                     readFromCSVFile();
+                    break;
+                case 11:
+                    readFromJSONFile();
                     break;
                 default:
                     status=false;
